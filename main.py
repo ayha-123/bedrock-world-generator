8c1ff8027f1c90849fb0bd0679867727027cc8ca
ayha-123 <ayhmhydrhakmalmhmwdy@gmail.com>
2026-09-26 15:13:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import math
 
 def main():
     if len(sys.argv) < 3:
@@ -8,10 +9,15 @@ def main():
     seed = int(sys.argv[1])
     world_size = int(sys.argv[2])
 
+    chunks_per_side = math.ceil(world_size / 16)
+    total_chunks = chunks_per_side * chunks_per_side
+
     print("Minecraft World Generator")
     print("=" * 60)
     print("SEED:", seed)
     print("WORLD SIZE:", world_size)
+    print("CHUNKS PER SIDE:", chunks_per_side)
+    print("TOTAL CHUNKS:", total_chunks)
     print("=" * 60)
 
 if __name__ == "__main__":
